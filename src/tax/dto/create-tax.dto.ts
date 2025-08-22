aee6ad2bf1ae25dfb0c01289aca5bd6fe17dd985
Nuriddin Mahmud <154328556+nuriddinmahmud@users.noreply.github.com>
2025-08-23 02:18:19 +0500

    taxbek

--- src/tax/dto/create-tax.dto.ts
+++ src/tax/dto/create-tax.dto.ts
@@ -1,14 +1,18 @@
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateTaxDto {
+  @ApiProperty({ example: 'Qiymat', description: '' })
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty({ example: 1500, description: 'Qanchaligi' })
   @IsNotEmpty()
   @IsNumber()
   amount: number;
 
+  @ApiPropertyOptional({ example: 'Paid in advance', description: 'Tavsifini' })
   @IsOptional()
   @IsString()
   comment?: string;
